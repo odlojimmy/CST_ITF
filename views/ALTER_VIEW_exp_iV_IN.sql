9a62b8ad415cd18e30d372bfff60d8ce1717c5ed
odlojimmy <30901195+odlojimmy@users.noreply.github.com>
2017-08-24 14:51:12 +0200

    Create ALTER_VIEW_exp_iV_IN.sql

---
+++ views/ALTER_VIEW_exp_iV_IN.sql
@@ -0,0 +1,34 @@
+/****** Object:  View [dbo].[exp_iV_IN]    Script Date: 21.08.2017 11:43:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+　
+ALTER VIEW [dbo].[exp_iV_IN]
+AS
+
+SELECT 'UPC' as UPC
+	,'SKU' as SKU
+	,'SupplierProductKey' as SupplierProductKey
+	,'EAN' as EAN
+	,'Inventory' as Inventory
+	,'Available_dt' as Available_dt
+	,'SupplierCatalogKey' as SupplierCatalogKey
+	,'InventorySource_cd' as InventorySource_cd
+
+UNION ALL
+
+SELECT [UPC]
+	,[SKU]
+	,[SupplierProductKey]
+	,[EAN]
+	,CAST([Inventory] AS VARCHAR(50))
+	,[Available_dt]
+	,[dbo].[GetProcPrm]('SupplierCatalogKeyPrefix',1)+[SupplierCatalogKey] as [SupplierCatalogKey] 
+	,[InventorySource_cd]
+FROM [dbo].[iV_IN]
+GO
+
+　
